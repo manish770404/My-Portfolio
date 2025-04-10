a53e16572e324f56e249bad65dd2756c06c347e6
manish jaiswal <mkjaiswal770408@gmail.com>
2025-04-10 19:43:36 +0530

    Added JSON support to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+// Add JSON support
+import json from '@rollup/plugin-json'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    json(), // Include JSON plugin for proper JSON imports
   ],
 })
